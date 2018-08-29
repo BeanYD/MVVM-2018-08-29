ad4ec14d08ff727ba7f5f518c90f693163b10922
dingbinbin <dingbinbin@dingbinbindemac-mini.local>
2018-08-29 14:40:14 +0800

    Initial Commit

---
+++ MVVM-2018-08-29/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM-2018-08-29
+//
+//  Created by dingbinbin on 2018/8/29.
+//  Copyright © 2018年 dingbinbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MVVM-2018-08-29Tests/MVVM_2018_08_29Tests.m
@@ -0,0 +1,39 @@
+//
+//  MVVM_2018_08_29Tests.m
+//  MVVM-2018-08-29Tests
+//
+//  Created by dingbinbin on 2018/8/29.
+//  Copyright © 2018年 dingbinbin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MVVM_2018_08_29Tests : XCTestCase
+
+@end
+
+@implementation MVVM_2018_08_29Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
